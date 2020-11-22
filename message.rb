5b78c1708af45e078277cc1aef1ef745c11cd4c3
unknown <agasievi@8tporchkhidze.utg.ge>
2020-11-22 20:09:35 +0400

    ver1_2

--- message.rb
+++ message.rb
@@ -5,7 +5,10 @@ get  '/' do
   erb :index
 end
 
-
 post  '/' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @vtime = params[:vdate]
+  @message = "Dear #{@name} , your phone #{@phone} come at #{@vtime}"
   erb :index
 end
