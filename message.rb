b83dd2f2064399c49a4ed498b8d17d62eb714f04
corax19 <agassiev1974@gmail.com>
2020-11-23 14:49:15 +0400

    1.0

--- message.rb
+++ message.rb
@@ -9,6 +9,11 @@ post  '/' do
   @name = params[:name]
   @phone = params[:phone]
   @vtime = params[:vdate]
+  myFile = File.open('./public/visits.txt', 'a')
+  strin1 = "#{@name};#{@phone};#{@vtime}"
+  myFile.puts strin1
+  myFile.close
+
   @message = "Dear #{@name} , your phone #{@phone} come at #{@vtime}"
   erb :index
 end
